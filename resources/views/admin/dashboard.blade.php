7ed65f94c62945de9dd3a33084b489d3490dcfb8
HimeshDua <himeshdua22@gmail.com>
2025-06-29 13:03:29 +0500

    some changes

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -24,6 +24,7 @@
             </div>
         </div>
 
+        {{-- Quick Actions  pleae work --}}
         <div class="mt-10 space-y-4">
             <h2 class="text-lg font-semibold text-foreground">Quick Actions</h2>
             <div class="flex flex-wrap gap-4">
