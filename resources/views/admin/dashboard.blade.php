a439e346001589056a5f53efdfff824d39ba2cca
HimeshDua <himeshdua22@gmail.com>
2025-06-29 13:19:46 +0500

    sync

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -25,7 +25,6 @@
         </div>
 
         {{-- Quick Actions  pleae work --}}
-        {{-- blahb blah blh --}}
         <div class="mt-10 space-y-4">
             <h2 class="text-lg font-semibold text-foreground">Quick Actions</h2>
             <div class="flex flex-wrap gap-4">
